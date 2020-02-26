dad52ccdc516172ab884daaf7cf9ea7545c1e824
KevinF <k.farrow@vrstudios.com>
2020-02-26 15:14:30 -0800

    up to 1.4

---
+++ Assets/Scripts/MovingSphere.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingSphere : MonoBehaviour
+{
+    [SerializeField, Range(0f, 100f)] float maxSpeed = 10f;
+    [SerializeField, Range(0f, 100f)] float maxAcceleration = 10f;
+
+    Vector3 velocity;
+    Rigidbody body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // Get player input
+        Vector2 playerInput;
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+        // Normalize the vector 
+        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+        // Calculate movement
+        velocity = body.velocity;
+        // Add speed
+        Vector3 desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        // Smooth the Acceleration
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+        velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+        // Apply movement
+        body.velocity = velocity;
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,1 @@
+httpscatlikecoding.comunitytutorialsmovementsliding-a-sphere
