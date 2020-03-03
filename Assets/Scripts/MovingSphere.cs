2146f3c8089c46435bc73ad30a04c5473f4a293f
KevinF <kevnaf@gmail.com>
2020-03-03 08:03:09 -0800

    3.5 lazy

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -4,23 +4,31 @@ using UnityEngine;
 
 public class MovingSphere : MonoBehaviour
 {
-    [SerializeField, Range(0f, 100f)] float maxSpeed = 10f;
+    [SerializeField, Range(0f, 10f)] float jumpHeight = 2f;
+    [SerializeField, Range(0f, 5)] int maxAirJumps = 2;
     [SerializeField, Range(0f, 100f)] float maxAcceleration = 10f;
     [SerializeField, Range(0f, 100f)] float maxAirAcceleration = 1f;
-    [SerializeField, Range(0f, 10f)] float jumpHeight = 2f;
-    [SerializeField, Range(0f, 90f)] float maxGroundAngle = 25f;
-    [SerializeField, Range(0f, 5)] int maxAirJumps = 2;
+    [SerializeField, Range(0f, 90f)] float maxGroundAngle = 40f;
+    [SerializeField, Range(0f, 100f)] float maxSpeed = 10f;
 
     bool onGround;
     bool triggerJump;
+    float minGroundDotProduct;
     int jumpPhase;
     Rigidbody body;
-    Vector3 velocity;
+    Vector3 contactNormal;
     Vector3 desiredVelocity;
+    Vector3 velocity;
+
+    private void OnValidate()
+    {
+        minGroundDotProduct = Mathf.Cos(maxGroundAngle * Mathf.Deg2Rad);
+    }
 
     private void Awake()
     {
         body = GetComponent<Rigidbody>();
+        OnValidate();
     }
 
     void Start()
@@ -78,12 +86,13 @@ public class MovingSphere : MonoBehaviour
         {
             jumpPhase++;
             float jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
+            float alignedSpeed = Vector3.Dot(velocity, contactNormal);
             // Limit how fast we can jump verticaly
-            if (velocity.y > 0f)
+            if (alignedSpeed > 0f)
             {
-                jumpSpeed = Mathf.Max(jumpSpeed - velocity.y, 0f);
+                jumpSpeed = Mathf.Max(jumpSpeed - alignedSpeed, 0f);
             }
-            velocity.y += jumpSpeed;
+            velocity += contactNormal * jumpSpeed;
         }
     }
 
@@ -92,7 +101,11 @@ public class MovingSphere : MonoBehaviour
         for (int i = 0; i < collision.contactCount; i++)
         {
             Vector3 normal = collision.GetContact(i).normal;
-            onGround |= normal.y >= 0.9f;
+            if (normal.y >= minGroundDotProduct)
+            {
+                onGround = true;
+                contactNormal = normal;
+            }
         }
     }
 
@@ -102,6 +115,10 @@ public class MovingSphere : MonoBehaviour
         if (onGround)
         {
             jumpPhase = 0;
+        }
+        else
+        {
+            contactNormal = Vector3.up;
         }
     }
 }
